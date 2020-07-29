bd54b992bd847a3366e7f1f77ea964e7e30c896b
Lucas Araujo <luquinhas.araujo26@hotmail.com>
2020-07-29 20:25:54 -0300

    Update server.js

--- bootback-master/models/server.js
+++ bootback-master/models/server.js
@@ -6,7 +6,7 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-routes.applyRoutes(server)
+routes.applyRoutes(server);
 
 server.listen(3000, function () {
     console.log('%s listening at %s', server.name, server.url);
